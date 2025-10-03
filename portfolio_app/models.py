b98a7da2550e87588fa0aa90d0438b82d68efbc9
fonckchain <afonck@protonmail.com>
2025-10-03 17:48:52 -0600

    Added views, URLs, and Bootstrap-styled template

---
+++ portfolio_app/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Skill(models.Model):
+    name = models.CharField(max_length=50)
+    proficiency = models.IntegerField()
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    technologies = models.ManyToManyField(Skill)
+    link = models.URLField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
