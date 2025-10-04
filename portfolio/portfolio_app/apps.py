32e01da09f31c2a7927155fc442bfcae77a95cc7
fonckchain <afonck@protonmail.com>
2025-10-03 20:28:06 -0600

    adjusted name

--- portfolio/portfolio_app/apps.py
+++ portfolio/portfolio_app/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 class PortfolioAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'portfolio.portfolio_app'
+    name = 'portfolio_app'
