628aa1e0256e2248db79fc541f8db5323c72cf47
fonckchain <afonck@protonmail.com>
2025-10-03 20:27:37 -0600

    Aligned app configuration and added admin interface

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'portfolio.portfolio_app',
+    'portfolio_app',
 ]
 
 MIDDLEWARE = [
